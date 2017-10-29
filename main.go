1a731cdc98fc4fe4cf1344c252ec149e5a381fce
Corby <corby@posteo.de>
2017-10-29 04:49:01 +0100

    refactor: Edit parameters for better readability

--- main.go
+++ main.go
@@ -30,10 +30,11 @@ Usage:
 	todo (<folder> ls| ls)
 	todo mkdir <folder>
 	todo rm <folder>
-	todo (<folder> head| head)
-	todo (<folder> cat [<name>]| cat [<name>])
-	todo (<folder> [(-d|--done)] <todo>| [(-d|--done)] <todo>)
-	todo (<folder> [(-u|--undo)] <todo>| [(-u|--undo)] <todo>)
+	todo (<folder> head | head)
+	todo (<folder> cat [<name>] | cat [<name>])
+	todo (<folder> <todo> | <todo>)
+	todo (<folder> (-d | --done) <todo>| (-d | --done) <todo>)
+	todo (<folder> (-u | --undo) <todo>| (-u | --undo) <todo>)
 	todo -h | --help
 	todo --version
 	`
